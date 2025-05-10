711634776b3bcf2d9b22bc1379554dd1ba3cd32c
BParker05 <braydenparker2701@gmail.com>
2025-05-11 00:22:42 +1000

    fixed app after I broke it dw about it

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,12 @@
+import { ThemedText } from '@/components/ThemedText';
 import { Link } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, } from 'react-native';
+import MapView, { Circle, Marker, PROVIDER_DEFAULT } from 'react-native-maps';
+
 
 export default function Screen1() {
   const RADIUS = 300;
 
-  // create markers and renderMarkers function
   const markers = [
     {
       coordinate: {
@@ -25,7 +27,7 @@ export default function Screen1() {
   ];
 
   const renderMarkers = () => {
-    return markers.map((marker, index) => (
+    return markers. map((marker, index) => (
       <Marker
         key={index}
         coordinate={marker.coordinate}
@@ -35,57 +37,32 @@ export default function Screen1() {
     ))
   }
 
-    // Reusable Button Component
-
-  const CustomButton = ({ onPress, text, link , style = {} }) => {
-    if (link) {
-      return (
-        <Link href={link} style={[styles.button, style]}>
-          <Text style={styles.buttonText}>{text}</Text>
-        </Link>
-      );
-    }
-
-    return (
-      <TouchableOpacity onPress={onPress} style={[styles.button, style]}>
-        <Text style={styles.buttonText}>{text}</Text>
-      </TouchableOpacity>
-    );
-  };
-
-  // 
   return(
   <View style={styles.container}>
-    <View style={styles.mapWrapper}>
-      <MapView
-        provider={PROVIDER_DEFAULT} // Map provider, google etc
+    <MapView
+      provider={PROVIDER_DEFAULT} // Map provider
 
-        style={styles.map}
-        initialRegion={{
-          latitude: -37.721,
-          longitude: 145.046,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-      >
-        {renderMarkers()}
-        <Circle center= {{
-          latitude: -37.721407,
-          longitude: 145.046530,
-        }} radius={200}>
-        </Circle>
-      </MapView>
-    </View>
+      style={styles.map}
+      initialRegion={{
+        latitude: -37.721,
+        longitude: 145.046,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    >
+      {renderMarkers()}
+      <Circle center= {{
+        latitude: -37.721407,
+        longitude: 145.046530,
+      }} radius={200}>
+      </Circle>
+    </MapView>
 
-    <CustomButton text="START/STOP" onPress={() => console.log('Start/Stop pressed')} />
-    <CustomButton text="Go to Screen 2" link="/screen2" />
-
-    {/* <ThemedText style={styles.button}>START/STOP</ThemedText>
-
-    <Link href="/screen2" style={styles.button}>
-      <ThemedText style={styles.buttonText}>Go to Screen 2</ThemedText>
-    </Link> */}
-
+    
+    <ThemedText style={styles.title}>Screen 1</ThemedText>
+      <Link href="/screen2" style={styles.button}>
+        <ThemedText style={styles.buttonText}>Go to Screen 2</ThemedText>
+      </Link>
   </View>
   );
 }
@@ -103,11 +80,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     color: 'white',
-    marginBottom: 0,
-    marginTop: 10,
+    marginBottom: 20,
   },
 
   button: {
+    marginTop: 20,
     padding: 10,
     backgroundColor: '#242424',
     borderWidth: 2,
@@ -116,7 +93,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     width: 200,
-    marginTop: 50,
   },
   buttonText: {
     color: 'white',
@@ -124,15 +100,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
 
-  // use mapWrapper to edit map's style
-  mapWrapper: {
-    width: '100%',
-    height: '50%',
-    borderRadius: 100,
-    marginTop: 150,
-
-  },
   map: {
+    flex: 1,
     width: '100%',
     height: '100%',
   },
