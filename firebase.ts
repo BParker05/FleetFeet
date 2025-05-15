21435d1aa4d756794c630f2e5ea41a921e0db278
BParker05 <braydenparker2701@gmail.com>
2025-05-15 21:42:58 +1000

    firebasets

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA-6AgCe-fiH0zQ7jmtASpG9qODWAKNwpc",
+  authDomain: "fleetfeet-c8d63.firebaseapp.com",
+  projectId: "fleetfeet-c8d63",
+  storageBucket: "fleetfeet-c8d63.appspot.com",
+  messagingSenderId: "367659769357",
+  appId: "1:367659769357:web:c5732554570e90825f3f44",
+  measurementId: "G-RRPH1M35GW",
 };
 
 // Initialize Firebase
